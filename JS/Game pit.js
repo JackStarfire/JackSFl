5021fa59d7367b1ce70d61139d9a151cf111ed01
JackStarfire <43181491+JackStarfire@users.noreply.github.com>
2019-01-10 10:45:49 -0700

    Add files via upload

---
+++ JS/Game pit.js
@@ -0,0 +1,2 @@
+var firstName = "Jack StarFire";
+alert("Welcome to the game pit just dont fall in ok.");
---
+++ JS/JackSF.com.js
@@ -0,0 +1,4 @@
+var firstName = "Jack StarFire";
+alert(firstName + " sees hello and thet if i dont know you take a hike");
+
+alert("Note:if Jack dusent know you the you have been slapd. so ya next time lisen");
